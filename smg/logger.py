bfcdc70e654f91a5a244408e7bba1acccc7a2880
songmingi <alsrl8@naver.com>
2022-02-28 01:53:49 +0000

    modify logger can log images

--- smg/logger.py
+++ smg/logger.py
@@ -1,4 +1,6 @@
 import wandb
+import PIL.Image
+from typing import Optional
 
 class MyLogger():
     def __init__(self) -> None:
@@ -6,6 +8,11 @@ class MyLogger():
         
     def log(self, data):
         wandb.log(data)
+
+    def log_images(self, key, images, caption:Optional[str] = None):
+        images = wandb.Image(images, caption)
+        wandb.log({key: images})
+        
     
     def close(self):
         wandb.finish()
@@ -17,5 +24,7 @@ if __name__ == '__main__':
     logger = MyLogger()
     logger.log({'test': 1})
     logger.log({'test': 2})
+    img = PIL.Image.open('/opt/ml/code/smg/data/test_image.png')
+    logger.log_images("test", img)  
     logger.close()
     
